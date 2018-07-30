936fab53d40370d3ba102d18b9dee8b8b4e7069d
camelvin <celvinacting@gmail.com>
2018-07-30 14:11:42 -0400

    Initial Commit

---
+++ wordeffects/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  wordeffects
+//
+//  Created by PHOENIXMAC on 2018-07-30.
+//  Copyright © 2018 Lighthouse Labs. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
