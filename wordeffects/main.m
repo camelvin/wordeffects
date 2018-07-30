41964e4a64d602fce98d3e9009ceac7638dcb393
camelvin <celvinacting@gmail.com>
2018-07-30 18:59:23 -0400

    got the switch working

--- wordeffects/main.m
+++ wordeffects/main.m
@@ -21,7 +21,11 @@ int main(int argc, const char * argv[]) {
         // 255 unit long array of characters
         char inputChars[255];
         int selector;
+        
         // infinite while loop
+       
+        
+        
         while (TRUE) {
             // repeat forever
             
@@ -30,23 +34,67 @@ int main(int argc, const char * argv[]) {
             // limit input to max 255 characters
             fgets(inputChars, 255, stdin);
             
-            // print as a c string
-            printf("Your string is %s\n", inputChars);
+           
+            
             
             //choices to choose from
-            printf("1.choice 1 \n");
-            printf("2.choice 2 \n");
-            printf("3.choice 3 \n");
-            printf("4.choice 4 \n");
-            printf("5.choice 5 \n");
-            printf("6.choice 6 \n");
+        
+            printf("Please choose and option.\n");
+           
+            printf("\t1. UpperCase \n");
+            printf("\t2. Lowercase \n");
+            printf("\t3. integer value.\n");
+            printf("\t4. Canadianize.\n");
+            printf("\t5. ? and ! answers.\n");
+            printf("\t6. replace spaces wirh - .\n");
+            //prompting user to choose answer 1/2/3 to the above answers
+            printf ("Enter a number between 1 and 6: ");
+            scanf("%i" , &selector);
+           
             
             // convert char array to an NSString object
             NSString *inputString = [NSString stringWithUTF8String:inputChars];
+            NSString *eh = @",eh.";
+            NSString *formattedString =[inputString stringByReplacingOccurrencesOfString:@" " withString:@"-"];
+
+            switch (selector) {
+                case 1:
+                    NSLog(@"%@",[inputString uppercaseString]);
+                    return 0;
+                    case 2:
+                    NSLog(@"%@",[inputString lowercaseString]);
+                    return 0;
+                case 3:
+                    NSLog(@"%i",[inputString intValue]);
+                    return 0;
+                case 4:
+                    NSLog(@"%@", [inputString stringByAppendingString:eh]);
+                    return 0;
+                case 5:
+                    if ([inputString containsString:@"?"]) {
+                        NSLog(@"I don't know");
+                        return 0;
+                    } else  if ([inputString containsString:@"!"]) {
+                        NSLog(@"whoa calm down");
+                        return 0;
+                        
+                    }
+                   return 0;
+                case 6:
+                    NSLog(@"%@",formattedString);
+                    return 0;
+                case 7:
+                    return 0;
+                    
+                default:
+                    NSLog(@"Please choose one" );
+                    break;
+            }
+      
             
             // print NSString object
             NSLog(@"Input was: %@", inputString);
-            // infinite while loop
+          
         }
     }
     return 0;
